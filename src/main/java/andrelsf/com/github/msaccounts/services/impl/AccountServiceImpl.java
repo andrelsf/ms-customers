db91909fc3ef9ef49c4ee2bedea95ca709af7cc2
Andre Ferreira <andre.dev.linux@gmail.com>
2024-08-04 20:31:37 -0300

    Implementacao da transferencia entre contas

--- src/main/java/andrelsf/com/github/msaccounts/services/impl/AccountServiceImpl.java
+++ src/main/java/andrelsf/com/github/msaccounts/services/impl/AccountServiceImpl.java
@@ -1,12 +1,20 @@
 package andrelsf.com.github.msaccounts.services.impl;
 
 import andrelsf.com.github.msaccounts.api.http.requests.Params;
+import andrelsf.com.github.msaccounts.api.http.requests.PostTransferRequest;
 import andrelsf.com.github.msaccounts.api.http.responses.AccountResponse;
+import andrelsf.com.github.msaccounts.entities.AccountEntity;
 import andrelsf.com.github.msaccounts.entities.AccountStatus;
+import andrelsf.com.github.msaccounts.entities.TransactionStatus;
+import andrelsf.com.github.msaccounts.entities.TransferEntity;
 import andrelsf.com.github.msaccounts.handlers.exceptions.AccountNotFoundException;
+import andrelsf.com.github.msaccounts.handlers.exceptions.UnableToTransfer;
 import andrelsf.com.github.msaccounts.repositories.AccountRepository;
+import andrelsf.com.github.msaccounts.repositories.TransferRepository;
 import andrelsf.com.github.msaccounts.services.AccountService;
 import andrelsf.com.github.msaccounts.utils.Mapper;
+import java.math.BigDecimal;
+import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -17,8 +25,10 @@ import org.springframework.transaction.annotation.Transactional;
 public class AccountServiceImpl implements AccountService {
 
   private final AccountRepository accountRepository;
+  private final TransferRepository transferRepository;
 
-  public AccountServiceImpl(AccountRepository accountRepository) {
+  public AccountServiceImpl(AccountRepository accountRepository, TransferRepository transferRepository) {
+    this.transferRepository = transferRepository;
     this.accountRepository = accountRepository;
   }
 
@@ -29,6 +39,42 @@ public class AccountServiceImpl implements AccountService {
         .stream()
         .map(Mapper::toAccountResponse)
         .collect(Collectors.toList());
+  }
+
+  @Override
+  @Transactional
+  public AccountResponse getTargetAccountBy(final UUID sourceAccountId, final PostTransferRequest request) {
+    return accountRepository.findByAgencyAndAccountNumber(request.agency(), request.accountNumber())
+        .map(Mapper::toAccountResponse)
+        .orElseThrow(() -> {
+          final TransferEntity transferEntity = TransferEntity.of(sourceAccountId, request, TransactionStatus.FAILED);
+          transferRepository.save(transferEntity);
+          return new UnableToTransfer(
+              "Unable to initiate transfer.\n Target account not found by agency and account number.");
+        });
+  }
+
+  @Override
+  @Transactional
+  public void processTransfer(String sourceAccountId, String targetAccountId, BigDecimal amount) {
+    AccountEntity sourceAccount = accountRepository.findById(sourceAccountId).get();
+    AccountEntity targetAccount = accountRepository.findById(targetAccountId).get();
+    final ZonedDateTime transferDate = ZonedDateTime.now();
+    sourceAccount.debit(amount, transferDate);
+    targetAccount.credit(amount, transferDate);
+  }
+
+  @Override
+  @Transactional
+  public AccountResponse getAccountForTransfer(final UUID accountId, final PostTransferRequest request) {
+    return accountRepository.findByIdAndBalanceGreaterThanEqual(accountId.toString(), request.amount())
+        .map(Mapper::toAccountResponse)
+        .orElseThrow(() -> {
+          final TransferEntity transferEntity = TransferEntity.of(accountId, request, TransactionStatus.FAILED);
+          transferRepository.save(transferEntity);
+          return new UnableToTransfer(
+              "Unable to initiate transfer.\n Please check your account balance and try again.");
+        });
   }
 
   @Override
--- src/main/java/andrelsf/com/github/msaccounts/utils/Mapper.java
+++ src/main/java/andrelsf/com/github/msaccounts/utils/Mapper.java
@@ -1,7 +1,12 @@
 package andrelsf.com.github.msaccounts.utils;
 
 import andrelsf.com.github.msaccounts.api.http.responses.AccountResponse;
+import andrelsf.com.github.msaccounts.api.http.responses.TransferResponse;
 import andrelsf.com.github.msaccounts.entities.AccountEntity;
+import andrelsf.com.github.msaccounts.entities.TransactionStatus;
+import andrelsf.com.github.msaccounts.entities.TransferEntity;
+import andrelsf.com.github.msaccounts.entities.TransferRecord;
+import java.time.ZonedDateTime;
 
 public class Mapper {
 
@@ -17,5 +22,26 @@ public class Mapper {
         accountEntity.getBalance(),
         accountEntity.getCreatedAt().toString(),
         accountEntity.getLastUpdated().toString());
+  }
+
+  public static TransferResponse toTransferResponse(final TransferEntity transfer) {
+    return new TransferResponse(
+        transfer.getId(),
+        transfer.getTargetAgency(),
+        transfer.getTargetAccountNumber(),
+        transfer.getAmount(),
+        transfer.getStatus().name(),
+        transfer.getTransferDate().toString());
+  }
+
+  public static TransferEntity recordToTransferEntity(final TransferRecord transferRecord) {
+    TransferEntity transferEntity = new TransferEntity();
+    transferEntity.setAccountId(transferRecord.accountId());
+    transferEntity.setTargetAgency(transferRecord.targetAgency());
+    transferEntity.setTargetAccountNumber(transferRecord.targetAccountNumber());
+    transferEntity.setAmount(transferRecord.amount());
+    transferEntity.setStatus(TransactionStatus.valueOf(transferRecord.status()));
+    transferEntity.setTransferDate(ZonedDateTime.now());
+    return transferEntity;
   }
 }
