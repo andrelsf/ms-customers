f4ab341756f822ef7a13544173b654e24fa7bff7
Andre Ferreira <andre.dev.linux@gmail.com>
2024-08-06 13:03:43 -0300

    Adicionado exceptionHandler para a exception customerNotFoundException

--- src/main/java/andrelsf/com/github/msaccounts/handlers/ApiRestHandlerExceptions.java
+++ src/main/java/andrelsf/com/github/msaccounts/handlers/ApiRestHandlerExceptions.java
@@ -1,5 +1,6 @@
 package andrelsf.com.github.msaccounts.handlers;
 
+import andrelsf.com.github.msaccounts.handlers.exceptions.CustomerNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -12,6 +13,13 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class ApiRestHandlerExceptions extends ResponseEntityExceptionHandler {
 
   private static final Logger log = LoggerFactory.getLogger(ApiRestHandlerExceptions.class);
+
+  @ExceptionHandler(CustomerNotFoundException.class)
+  protected ResponseEntity<ApiErrorResponse> handleCustomerNotFoundException(CustomerNotFoundException ex) {
+    log.error(ex.getMessage(), ex);
+    return ResponseEntity.status(HttpStatus.NOT_FOUND)
+        .body(new ApiErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage()));
+  }
 
   @ExceptionHandler(RuntimeException.class)
   protected ResponseEntity<ApiErrorResponse> handlerRuntimeException(RuntimeException ex) {
